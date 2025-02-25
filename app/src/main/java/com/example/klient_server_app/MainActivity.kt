000f994dbb6160ce167e1f18e183210c14b10545
Vorontsov Sergey <sergeyvorontsov2018@yandex.ru>
2025-02-26 00:53:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/klient_server_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.klient_server_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.klient_server_app.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
